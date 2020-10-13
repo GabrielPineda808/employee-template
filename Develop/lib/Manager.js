b65da1406f9bc9373e8248aa5f8f92119669cfbd
Gabriel Pineda <gabepineda6@gmail.com>
2020-10-13 14:27:28 -0700

    added a constructor which takes the name, email, and id plus a new office number parameter and changed the roel to manager

--- Develop/lib/Manager.js
+++ Develop/lib/Manager.js
@@ -1,7 +1,11 @@
 // TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
 const Employee = require("./Employee.js");
 class Manager extends Employee{
-    
+    constructor(name, id, email, office){
+        super(name, id, email);
+        this.office = office;
+        this.role = "Manager";
+    }
 }
 
 module.exports = Manager;
