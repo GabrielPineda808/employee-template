f5475ef20688e54424c1964229634c732533c1ad
Gabriel Pineda <gabepineda6@gmail.com>
2020-10-13 20:24:09 -0700

    Created an inquirer prompt that shows a list of roles you can choose from

--- Develop/app.js
+++ Develop/app.js
@@ -13,7 +13,27 @@ const render = require("./lib/htmlRenderer");
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+inquirer
+  .prompt([
+    {
+      type: "list",
+      message: "What is the role you are adding?",
+      name: "type",
+      choices: [
+        { name: 'Manager'},
+        { name: 'Engineer'},
+        { name: 'Intern' },
+        { name: 'No More To Add'},
+      ]
+    }
+  ])
+  .then(res => {
+      switch(res.type){
+        
+      }
+  }
 
+  );
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
